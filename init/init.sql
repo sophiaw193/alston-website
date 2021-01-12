ba29e4831aa07cc13ed4ecdfb260657c0f2f5498
aw777 <aw777@cornell.edu>
2021-01-11 19:29:29 -0500

    commit

--- init/init.sql
+++ init/init.sql
@@ -8,26 +8,36 @@ CREATE TABLE posts (
 
 
 INSERT INTO posts (title, cal, preview, content) VALUES (
-	'A Fresh Start', 
-	'Dec 29, 2020', 
-	'As 2021 draws closer, a common question that we might get asked is: “What is your New Year’s resolution?”...', 
+	'A Fresh Start',
+	'Dec 29, 2020',
+	'As 2021 draws closer, a common question that we might get asked is: “What is your New Year’s resolution?”...',
 	'As 2021 draws closer, a common question that we might get asked is: “What is your New Year’s resolution?”
 	<br/><br/>
 	Now, take a moment and think about what you said at the end of last year...
 	<br/><br/>
 	Well, to be frank, I don’t remember the last time I set a New Year’s resolution for myself. And if I did, it would probably be back in high school where I made it my goal to climb the ladders in a video game called League of Legends. (I did peak at Diamond Four, if you were even remotely curious)
 	<br/><br/>
-	Over the years, I have stopped making New Year’s resolutions. It is not because I sucked at accomplishing them, but rather I have become aware of what separates a resolution from just another goal on my bucket list. 
-	I have some goals on my bucket list, including 1) backpacking in Southeast Asia 2) producing a short film and 3) flying around to play in EPTs (European Poker Tour). The list is only growing as I am getting older. However, I began to notice that different goals on my bucket list required different levels of time, health, and money. 
+	Over the years, I have stopped making New Year’s resolutions. It is not because I sucked at accomplishing them, but rather I have become aware of what separates a resolution from just another goal on my bucket list.
+	I have some goals on my bucket list, including 1) backpacking in Southeast Asia 2) producing a short film and 3) flying around to play in EPTs (European Poker Tour). The list is only growing as I am getting older. However, I began to notice that different goals on my bucket list required different levels of time, health, and money.
 	For example, backpacking in Southeast Asia will take a dedicated amount of time, a healthy body, and relatively smaller amounts of money to complete. Since I should have the most time and the best health in my younger years, I figured I probably should get to that as soon as possible.
 	At the same time, I look at other goals of producing a short film and attending poker tournaments as goals that I can accomplish even with deteriorating health in my later years. Therefore, these goals are set at the very bottom of the bucket.
-	You might ask what the heck is this guy blabbering about and what does all this have to do with a New Year’s resolution? Well, I think that these resolutions are inhibiting our ability to perform more. 
-	One of my friends had made it his New Year’s resolution to eat healthier so he could lose weight. However, after two and a half weeks of “healthy eating,” he gave out and resumed his diet from the previous year. Afterward, he told himself that he had already failed his resolution for the year and that he would try again next year, hopefully coming back stronger. 
-	This mentality of attaching a goal to a whole year of 365 days - or 366 days every four years if you really want to technical - is problematic. According to a report from Forbes, around 80% of Americans fail their New Year’s Resolutions. 
-	As humans, we are taught that failure builds success. However, this belief provides us with the cushion which ultimately leaves us too much room for failure. 
+	You might ask what the heck is this guy blabbering about and what does all this have to do with a New Year’s resolution? Well, I think that these resolutions are inhibiting our ability to perform more.
+	One of my friends had made it his New Year’s resolution to eat healthier so he could lose weight. However, after two and a half weeks of “healthy eating,” he gave out and resumed his diet from the previous year. Afterward, he told himself that he had already failed his resolution for the year and that he would try again next year, hopefully coming back stronger.
+	This mentality of attaching a goal to a whole year of 365 days - or 366 days every four years if you really want to technical - is problematic. According to a report from Forbes, around 80% of Americans fail their New Year’s Resolutions.
+	As humans, we are taught that failure builds success. However, this belief provides us with the cushion which ultimately leaves us too much room for failure.
 	Instead, I think that we should be actively aware of our resolutions and goals and be able to differentiate them. By no means am I trying to convince you that a New Year’s resolution is worthless...But I am trying to convince you to be articulate and cautious of what you set your New Year’s resolution to be. I hope that your takeaway from this is to just do and don’t think.
 	2020 has been a tough year for all of us. I hope that 2021 brings us a fresh start and the future shines a bright light.
 	Cheers,
 	Alston'
 	);
 
+INSERT INTO posts (title, cal, preview, content) VALUES (
+    'January 6th, 2021',
+    'January 6th, 2021',
+    '',
+    '2021 may have just started, but chaos remains imminent in the United States. As a Pro-Trump mob breached the Capitol Building on this day, the whole world is closely observing as the incident escalates.
+    Although I would not categorize myself as a liberal or conservative, some of my values and morals are reflected upon the policies of the left (pro-choice, pro-gay-marriage, pro-environment, just to name a few). At the same time, I recognize that while I do agree upon ideologies and policies that resemble the Democratic party, I could be a victim of an echo chamber from the prevalence of social media technologies. As a citizen, I take the freedom of free-thinking for granted.
+    Therefore, I am meticulous and aware of every step on social media because I do firmly believe that in order to formulate a justified opinion on a topic, one needs to be first informed of the facts of both sides. I was having dinner with my mother earlier today and she was telling me about how the Democrats rigged the election (keep in mind that my mom is an immigrant with limited English skills who gets most of her news source from WeChat). What was intriguing from this conversation was how much of the news over WeChat reflected upon right-wing media. I was skeptical but did not want to debunk her statements because like I said earlier in this post, in order to make justified opinions, one needs to be informed of both sides. The truth was that I was not completely familiar with the stories she shared because I was simply not exposed to any of the same sources on the platforms that I use. What I did suggest to her is for her to expose herself to videos and stories from the other side (although that was an impossible task because most, if not all, Chinese-reported news on WeChat were pro-Trump, which brings the question: Why are Chinese sources on WeChat only bringing attention to right-wing messages and ideas?) ….What comes to mind is how powerful technology has become in our lives. It has the power to influence communities of people on a global scale almost as if it was a form of brainwashing. In fact, the closest thing that comes remotely close to social media was the mass usage of television in homes back in the mid 20th century. And now it is time to address the ethical concerns and morality concerns.
+    Netflix original, titled Social Dilemma, tackled this problem (highly recommend if you have not watched it). The documentary addressed the problem of social media manipulating lives and creating a divisive world through the means of revenue. Now, technology and social media play a bigger role than ever, not only in our daily lives but also in the economy as well as politics.
+    The arrival of social media has perpetuated the idea of pushing an agenda through technology. In many ways, we are active consumers of the propaganda formulated on the networks around us. In fact, sensational content will always grab more attention than serious journalism, as shown in the past again and again. However, integrity should never be exchanged for attention. The scariest part is that the technology companies are really the only mediators who can save us from the mess that they have created...'
+    );

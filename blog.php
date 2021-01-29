3a03441877570ce15912ae3489ded46743a523ab
Sophia Wang <sw847@cornell.edu>
2021-01-29 00:20:38 -0500

    life

--- blog.php
+++ blog.php
@@ -24,8 +24,8 @@ $db = open_or_init_sqlite_db('posts.sqlite', "init/init.sql");
         <h2 class="name">ALSTON WANG</h2>
         <div>
             <ul class="links">
-                <li class="links"><a href="portfolio.html">WORK</a></li>
-                <li class="links"><a href="discovery.html">INTERESTS</a></li>
+                <li class="links"><a href="work.html">WORK</a></li>
+                <li class="links"><a href="life.html">LIFE</a></li>
                 <li class="links"><a class="active" href="blog.php">BLOG</a></li>
                 <li class="links"><a href="about.html">ABOUT</a></li>
             </ul>
@@ -41,7 +41,7 @@ $db = open_or_init_sqlite_db('posts.sqlite', "init/init.sql");
             $records = exec_sql_query($db, "SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
             foreach ($records as $record) {
                 echo
-                    "
+                "
             <div class=one-post>
             <a href=\"post.php?id=" . $record["id"] . "\">
             <h3>" . $record["title"] . "</h3>
